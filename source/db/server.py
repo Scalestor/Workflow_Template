6effdb74f63567722d4d6cb262a3a1f728fff715
Malte Schroeder <malte_schroeder@outlook.com>
2025-02-27 21:46:11 +0100

    Setup of the flask backend server

---
+++ source/db/server.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine, inspect
+from sqlalchemy.orm import  sessionmaker
+
+from config.database import database_engine_string
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    # Example business logic
+    engine = create_engine(database_engine_string, echo=True)
+    inspector = inspect(engine)
+    tables = inspector.get_table_names()
+    return jsonify({"message": f"Data fetched from backend! {tables}"})
+
+#@app.route('/api/process', methods=['POST'])
+#def process_data():
+    # Example business logic for processing data sent by frontend
+    #data = request.json
+    #result = {"processed_data": f"Processed: {data.get('value')}"}
+    #return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=5001)  # Backend server runs on port 5001
---
+++ source/db/tools/metadata_tools.py
@@ -0,0 +1,5 @@
+from config.database import database_engine_string
+
+from sqlalchemy import create_engine, inspect
+from sqlalchemy.orm import  sessionmaker
+
