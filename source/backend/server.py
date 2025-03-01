e3d2fde00cd08e62ae2bcd531b68bf8dd35dce24
Malte Schroeder <malte_schroeder@outlook.com>
2025-03-01 17:49:43 +0100

    Improvement in internal print statements as well as the extraction of the number of entries in the backend.

--- source/backend/server.py
+++ source/backend/server.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine, inspect
+from sqlalchemy import create_engine, inspect,text
 from sqlalchemy.orm import  sessionmaker
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 
 
 
@@ -9,20 +9,44 @@ from source.config import database
 
 app = Flask(__name__)
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    # Example business logic
-    engine = create_engine(database.database_engine_string, echo=True)
-    inspector = inspect(engine)
-    tables = inspector.get_table_names()
-    return jsonify({"message": tables})
+allowed_resources = ["data","tables"]
 
-#@app.route('/api/process', methods=['POST'])
-#def process_data():
-    # Example business logic for processing data sent by frontend
-    #data = request.json
-    #result = {"processed_data": f"Processed: {data.get('value')}"}
-    #return jsonify(result)
+@app.before_request
+def log_request():
+    print(f"Incoming request: {request.method} {request.url} 'Args' {request.args} 'Data' {request.data}")
+
+@app.route('/api/<resource>', methods=["GET","POST"])
+def handle_request(resource):
+    if resource not in allowed_resources:
+        return jsonify({"error": "Resource not found"}), 404
+
+    if resource == "tables":
+        engine = create_engine(database.database_engine_string, echo=True)
+        inspector = inspect(engine)
+        tables = inspector.get_table_names()
+        return jsonify({"message": tables})
+    
+    if resource == "data":
+        engine = create_engine(database.database_engine_string, echo=True)
+        inspector = inspect(engine)
+        tables = inspector.get_table_names()
+        look_for_table = request.json["table"]
+
+        if look_for_table not in tables:
+            return jsonify({"error": f"Table {look_for_table} not found. Expected {request.json}. Have tables {tables}"}), 404
+        
+        with engine.connect() as connection:
+            query = text(f"SELECT COUNT(*) FROM {look_for_table} ")
+            result = connection.execute(query).scalar()
+            response = {"message": f"Data from query {query}" , "data":result}
+            print(f"Message built: {response}")
+            #rows = [dict(row) for row in result.mappings()]  # Convert to JSON format
+            return jsonify(response)
+        
+
+   
+
+
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5001)  # Backend server runs on port 5001
